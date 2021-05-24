6ccf022d0c2b14d3282120c5a91fd2ba13f31970
Shourya dev atrey <shouryadevatrey@gmail.com>
2021-05-24 20:41:23 +0530

    Added client-react

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import {Admin, Resource} from "react-admin";
+import restProvider from 'ra-data-simple-rest';
+import PostList from './components/PostList'
+
+function App() {
+  return (
+    <Admin dataProvider={restProvider('http://localhost:3000')}>
+      <Resource name="posts" list={PostList} />
+    </Admin>
+  );
+}
+
+export default App;
